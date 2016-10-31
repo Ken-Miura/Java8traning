bf16d3bb711561c9af9033b46a909f05923b6df5
Ken-Miura <ken.miura1102@gmail.com>
2016-11-01 01:22:29 +0900

    引数の配列の中身のチェック追加

--- src/ch01/ex04/FileSorter.java
+++ src/ch01/ex04/FileSorter.java
@@ -8,6 +8,9 @@ public final class FileSorter {
 
 	public static void sortFile (File[] files) {
 		Objects.requireNonNull(files, "files must not be null");
+		for (final File file: files) {
+			Objects.requireNonNull(file, "file must not be null");	
+		}
 		Arrays.sort(files, (file1, file2)->{
 			// 両方ファイルか両方ディレクトリかの検査
 			if (file1.isDirectory() == file2.isDirectory()) {

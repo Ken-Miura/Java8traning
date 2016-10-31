ae64e316ebc1f9639fab6c89363b2261859861f8
Ken-Miura <ken.miura1102@gmail.com>
2016-10-31 21:43:47 +0900

    練習問題追加

---
+++ src/ch01/ex04/FileSorter.java
@@ -0,0 +1,19 @@
+package ch01.ex04;
+
+import java.io.File;
+import java.util.Arrays;
+import java.util.Objects;
+
+public final class FileSorter {
+
+	public static void sortFile (File[] files) {
+		Objects.requireNonNull(files, "files must not be null");
+		Arrays.sort(files, (file1, file2)->{
+			// 両方ファイルか両方ディレクトリかの検査
+			if (file1.isDirectory() == file2.isDirectory()) {
+				return file1.compareTo(file2);
+			}
+			return file1.isDirectory()? -1:1;
+		});
+	}
+}

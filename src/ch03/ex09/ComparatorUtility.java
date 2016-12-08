ecb293e6a799d3447b606fd588a2ad8821a95e62
Ken-Miura <ken.miura1102@gmail.com>
2016-12-09 03:46:38 +0900

    例外処理を簡潔にして少しだけ読みやすく変更

--- src/ch03/ex09/ComparatorUtility.java
+++ src/ch03/ex09/ComparatorUtility.java
@@ -17,30 +17,19 @@ public final class ComparatorUtility {
 			for (final String fieldName: fieldNames) {
 				try {
 					Field f1 = obj1.getClass().getDeclaredField(fieldName);
-					Comparable<? super Comparable<?>> c1 = null;
-					try {
-						f1.setAccessible(true);
-						@SuppressWarnings("unchecked")
-						Comparable<? super Comparable<?>> temp = (Comparable<? super Comparable<?>>) f1.get(obj1);
-						c1 = temp;
-					} catch (IllegalArgumentException | IllegalAccessException e) {
-						throw new RuntimeException(e);	
-					}
+					f1.setAccessible(true);
+					@SuppressWarnings("unchecked")
+					Comparable<? super Comparable<?>> c1 = (Comparable<? super Comparable<?>>) f1.get(obj1);
 					
 					Field f2 = obj2.getClass().getDeclaredField(fieldName);
-					Comparable<?> c2 = null;
-					try {
-						f2.setAccessible(true);
-						c2 = (Comparable<?>) f2.get(obj2);
-					} catch (IllegalArgumentException | IllegalAccessException e) {
-						throw new RuntimeException(e);	
-					}
+					f2.setAccessible(true);
+					Comparable<?> c2 = (Comparable<?>) f2.get(obj2);
 					
 					int result = c1.compareTo(c2);
 					if (result != 0) {
 						return result;
 					}
-				} catch (NoSuchFieldException | SecurityException e) {
+				} catch (NoSuchFieldException | SecurityException | IllegalArgumentException | IllegalAccessException e) {
 					throw new RuntimeException(e);
 				}
 			}

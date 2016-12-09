f99deca51ae479637d6aacb6573879024983ae3b
Ken-Miura <ken.miura1102@gmail.com>
2016-12-10 04:05:12 +0900

    練習問題追加

---
+++ src/ch03/ex13/BlurImage.java
@@ -0,0 +1,82 @@
+/**
+ * Copyright 2016 Ken Mirua
+ */
+package ch03.ex13;
+
+import java.util.List;
+
+import ch03.ex13.ImageUtility.ConvolutionFilter;
+import javafx.application.Application;
+import javafx.scene.Group;
+import javafx.scene.Scene;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Alert.AlertType;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.paint.Color;
+import javafx.stage.Stage;
+
+public final class BlurImage extends Application {
+
+	@Override
+	public void start(Stage primaryStage) throws Exception {
+		Parameters params = getParameters();
+		List<String> args = params.getRaw();
+		if (args.size() != 1) {
+			Alert alert = new Alert(AlertType.ERROR);
+			alert.setTitle("Error");
+			alert.setHeaderText("Error");
+			alert.setContentText("プログラム起動時に画像ファイルのURIを引数に与えてください。");
+			alert.showAndWait();
+			System.exit(0);
+		}
+		ImageView imageView = new ImageView();
+		Image image = new Image(args.get(0));
+
+		ConvolutionFilter blur = (x, y, matrix) -> {
+			int size = matrix.length * matrix[0].length;
+			double redSum = 0.0;
+			for (Color[] row :matrix) {
+				for (Color color: row) {
+					redSum+=color.getRed();
+				}	
+			}
+			double greenSum = 0.0;
+			for (Color[] row :matrix) {
+				for (Color color: row) {
+					greenSum+=color.getGreen();
+				}	
+			}
+			double blueSum = 0.0;
+			for (Color[] row :matrix) {
+				for (Color color: row) {
+					blueSum+=color.getBlue();
+				}	
+			}
+			return Color.color(redSum/size, greenSum/size, blueSum/size);
+		};
+		
+		Image transformedImage = LatentImage.from(image)
+									.transform(blur)
+									.transform(blur)
+									.transform(blur)
+									.transform(blur)
+									.transform(blur)
+									.toImage();
+		
+		imageView.setImage(transformedImage);
+
+		Group root = new Group();
+		root.getChildren().add(imageView);
+		Scene scene = new Scene(root);
+
+	    primaryStage.setTitle(BlurImage.class.getSimpleName());
+	    primaryStage.setScene(scene);
+	    primaryStage.show();
+	}
+
+	public static void main(String[] args) {
+		Application.launch(args);
+	}
+
+}
---
+++ src/ch03/ex13/LatentImage.java
@@ -0,0 +1,83 @@
+/**
+ * Copyright 2016 Ken Mirua
+ */
+package ch03.ex13;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+import java.util.function.UnaryOperator;
+
+import ch03.ex13.ImageUtility.ColorTransformer;
+import ch03.ex13.ImageUtility.ConvolutionFilter;
+import javafx.scene.image.Image;
+import javafx.scene.image.WritableImage;
+import javafx.scene.paint.Color;
+
+public final class LatentImage {
+
+	private Image in;
+	private final List<ConvolutionFilter> pendingOperations = new ArrayList<>();
+	
+	private LatentImage (Image in) {
+		Objects.requireNonNull(in, "in must not be null");
+		this.in = in;
+	}
+	
+	public static LatentImage from (Image image) {
+		return new LatentImage(image);
+	}
+	
+	public LatentImage transform (ColorTransformer f) {
+		Objects.requireNonNull(f, "f must not be null");
+		pendingOperations.add((x, y, matrix)->{ return f.apply(x, y, matrix[1][1]); });
+		return this;
+	}
+	
+	public LatentImage transform (UnaryOperator<Color> f) {
+		return transform(toColorTransformer(f));
+	}
+	
+	public LatentImage transform (ConvolutionFilter f) {
+		Objects.requireNonNull(f, "f must not be null");
+		// 前段までのものを強制
+		if (pendingOperations.size() != 0) {
+			in = toImage();
+			pendingOperations.clear();
+		}
+		pendingOperations.add(f);
+		return this;
+	}
+	
+	private ColorTransformer toColorTransformer(UnaryOperator<Color> op) {
+		return (x, y, c)->{ return op.apply(c); };
+	}
+	
+	public Image toImage () {
+		int width = (int) in.getWidth();
+		int height = (int) in.getHeight();
+		WritableImage temp = new WritableImage(1+width+1, 1+height+1); // 畳みこみ操作のために左右上下に１ピクセル多く確保
+		for (int x=0; x<width; x++) {
+			for (int y=0; y<height; y++) {
+				temp.getPixelWriter().setColor(x+1, y+1, in.getPixelReader().getColor(x, y));
+			}
+		}
+		
+		for (int x=0; x<width; x++) {
+			for (int y=0; y<height; y++) {
+				Color c = temp.getPixelReader().getColor(x+1, y+1);
+				Color[][] matrix = new Color[3][3];
+				for (int i=0; i<matrix.length; i++) {
+					for (int j=0; j<matrix[0].length; j++) {
+						matrix[i][j] = temp.getPixelReader().getColor(x+i, y+j);
+					}
+				}
+				for (final ConvolutionFilter f: pendingOperations) {
+						c = f.apply(x, y, matrix);
+				}
+				temp.getPixelWriter().setColor(x+1, y+1, c);
+			}
+		}
+		return new WritableImage(temp.getPixelReader(), 1, 1, width, height);
+	}
+}

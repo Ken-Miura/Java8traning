b151e59d8ce199d8151d0c60db092b1070682bfd
Ken-Miura <ken.miura1102@gmail.com>
2016-12-17 00:32:00 +0900

    テスト追加

--- test/ch03/ex01/DelayedLoggerTest.java
+++ test/ch03/ex01/DelayedLoggerTest.java
@@ -1,13 +1,29 @@
 package ch03.ex01;
 
 
+import java.util.concurrent.atomic.AtomicInteger;
 import java.util.logging.Level;
 import java.util.logging.Logger;
+import static org.junit.Assert.assertThat;
+import static org.hamcrest.CoreMatchers.is;
 
 import org.junit.Test;
 
 public class DelayedLoggerTest {
 
+	@Test
+	public void logIf_EvaluatesConditionIfLogging() {
+		final Logger logger = Logger.getLogger(DelayedLogger.class.getName());
+		logger.setLevel(Level.FINEST);
+		
+		final AtomicInteger passCounter = new AtomicInteger();
+		final DelayedLogger delayedLogger = new DelayedLogger(logger);
+		delayedLogger.logIf(Level.FINEST, 
+				 ()->{ synchronized(passCounter) { passCounter.getAndIncrement(); } return true;}, 
+				 ()->{ synchronized(passCounter) { passCounter.getAndIncrement(); } return "message";});
+		assertThat(passCounter.get(), is(2));
+	}
+	
 	@Test
 	public void logIf_DoesNotEvaluateConditionIfNotLogging() {
 		final Logger logger = Logger.getLogger(DelayedLogger.class.getName());

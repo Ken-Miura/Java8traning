ef4798d1e81d768a1a76d83bffb6be9546d7ff3e
Ken-Miura <ken.miura1102@gmail.com>
2016-12-17 00:31:13 +0900

    TODO追加

--- src/ch04/ex08/LoginDialog.java
+++ src/ch04/ex08/LoginDialog.java
@@ -11,6 +11,7 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
+// Java FXのコントロールではなく、普通のJavaのオブジェクトをfxmlから作ることがこの問題の趣旨らしいのでこの回答は間違い。時間取れれば試したい。TODO
 public final class LoginDialog extends Application  {
 
 	public static void main (String... args) {

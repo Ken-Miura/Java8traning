16129f46b3f51b9aa81f40f89e4ed8ccc52609cb
Ken-Miura <ken.miura1102@gmail.com>
2017-01-14 23:40:20 +0900

    微修正

--- src/ch05/ex08/ListUTCTimeOffsets.java
+++ src/ch05/ex08/ListUTCTimeOffsets.java
@@ -5,13 +5,11 @@ package ch05.ex08;
 
 import java.time.ZoneId;
 import java.time.ZonedDateTime;
-import java.util.stream.Stream;
 
 public final class ListUTCTimeOffsets {
 
 	public static void main(String[] args) {
-		final Stream<String> zoneIdsStream = ZoneId.getAvailableZoneIds().stream();
-		zoneIdsStream.forEach(zoneIdString->{
+		ZoneId.getAvailableZoneIds().stream().forEach(zoneIdString->{
 			ZonedDateTime now = ZonedDateTime.now(ZoneId.of(zoneIdString));
 			System.out.println("Time zone: " + zoneIdString + ", UTC time offset: " + now.getOffset());
 		});

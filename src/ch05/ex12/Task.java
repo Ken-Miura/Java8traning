391d3996b525eb20fd1cc8bad2a65fbbbac994bf
Ken-Miura <ken.miura1102@gmail.com>
2017-01-20 22:32:59 +0900

    メンバ変数名をシンプルに

--- src/ch05/ex12/Task.java
+++ src/ch05/ex12/Task.java
@@ -8,21 +8,21 @@ import java.util.Objects;
 
 final class Task {
 
-	private final String taskName;
-	private final ZonedDateTime taskTime;
+	private final String name;
+	private final ZonedDateTime time;
 	
 	Task(String taskName, ZonedDateTime taskTime) {
 		Objects.requireNonNull(taskName, "taskName must not be null");
 		Objects.requireNonNull(taskTime, "taskTime must not be null");
-		this.taskName = taskName;
-		this.taskTime = taskTime;
+		this.name = taskName;
+		this.time = taskTime;
 	}
 
-	final String getTaskName() {
-		return taskName;
+	final String getName() {
+		return name;
 	}
 
-	final ZonedDateTime getTaskTime() {
-		return taskTime;
+	final ZonedDateTime getTime() {
+		return time;
 	}
 }

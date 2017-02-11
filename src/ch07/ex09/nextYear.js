7d1469b3d0fb58bd6dd06746a99bd0aeb2811631
Ken-Miura <ken.miura1102@gmail.com>
2017-02-11 21:55:14 +0900

    バグ修正

--- src/ch07/ex09/nextYear.js
+++ src/ch07/ex09/nextYear.js
@@ -1,10 +1,11 @@
-#!C:\Program Files\Java\jre1.8.0_102\bin\jrunscript.exe -f
+#!/home/kmiura/java/jdk1.8.0_121/bin/jjs
+
 var text = "Next year, you wil be ";
 if (arguments!=0) {
-	print(text + arguments[0]);
+	print(text + (Number(arguments[0])+1));
 } else if (typeof $ENV.AGE !== "undefined") {
-	print(text + $ENV.AGE);
+	print(text + (Number($ENV.AGE)+1));
 } else {
 	var age = readLine("Your age: ");
-	print(text + age);
+	print(text + (Number(age)+1));
 }

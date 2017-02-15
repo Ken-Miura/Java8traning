8967fe9638cb187e30c25e5cb95707f23329197d
Ken-Miura <ken.miura1102@gmail.com>
2017-02-16 03:34:19 +0900

    バグ修正

--- src/ch08/ex12/TestedClass.java
+++ src/ch08/ex12/TestedClass.java
@@ -24,6 +24,6 @@ public final class TestedClass {
 	@TestCase(params=4, expected=24)
 	@TestCase(params=0, expected=1)
 	public static int factorial(int n) {
-		return n==0 ? 1 : n*factorial(n-1);
+		return n<=1 ? 1 : n*factorial(n-1);
 	}
 }

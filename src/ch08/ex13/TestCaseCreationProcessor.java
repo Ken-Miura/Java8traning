e0f8a8f3c7f099395082f799d54898352273da4b
Ken-Miura <ken.miura1102@gmail.com>
2017-02-17 02:10:51 +0900

    動作確認済

--- src/ch08/ex13/TestCaseCreationProcessor.java
+++ src/ch08/ex13/TestCaseCreationProcessor.java
@@ -8,17 +8,18 @@ import java.io.OutputStream;
 import java.io.OutputStreamWriter;
 import java.io.Writer;
 import java.nio.charset.StandardCharsets;
+import java.util.HashSet;
 import java.util.Set;
 
 import javax.annotation.processing.AbstractProcessor;
 import javax.annotation.processing.Filer;
 import javax.annotation.processing.RoundEnvironment;
+import javax.lang.model.SourceVersion;
 import javax.lang.model.element.Element;
 import javax.lang.model.element.TypeElement;
 
 import ch08.ex13.TestCase;
 
-// 動作未確認
 public final class TestCaseCreationProcessor extends AbstractProcessor {
 
 	@Override
@@ -29,20 +30,20 @@ public final class TestCaseCreationProcessor extends AbstractProcessor {
 					Writer writer = new OutputStreamWriter(outputStream, StandardCharsets.UTF_8);) {
 				writer.write("package ch08.ex13;" + System.lineSeparator());
 				writer.write("public final class Test {" + System.lineSeparator());
-				writer.write("public static void main (String[] args) throws Exception {" + System.lineSeparator());
+				writer.write("\tpublic static void main (String[] args) throws Exception {" + System.lineSeparator());
 				
 				for (final Element element : roundEnv.getElementsAnnotatedWith(annotation)) {
 					final String method = element.getEnclosingElement() + "." + element.getSimpleName();
 					TestCase[] testCases = element.getAnnotationsByType(TestCase.class);
                     for (final TestCase testCase : testCases) {
-                    	writer.write("if(" + method + "(" + testCase.params() + ") != " + testCase.expected() + ") {");
-                    	writer.write("throw new AssertionError();");
-                    	writer.write("}"); // if
+                    	writer.write("\t\tif(" + method + "(" + testCase.params() + ") != " + testCase.expected() + ") {" + System.lineSeparator());
+                    	writer.write("\t\t\tthrow new AssertionError();" + System.lineSeparator());
+                    	writer.write("\t\t}" + System.lineSeparator()); // if
                     }
-					
+
 				}
-				
-				writer.write("}" + System.lineSeparator()); // mainメソッド
+				writer.write("\t\tSystem.out.println(\"All the tests passed.\");" + System.lineSeparator()); // mainメソッド
+				writer.write("\t}" + System.lineSeparator()); // mainメソッド
 				writer.write("}" + System.lineSeparator()); // Testクラス
 			} catch (IOException e) {
 				e.printStackTrace();
@@ -50,4 +51,18 @@ public final class TestCaseCreationProcessor extends AbstractProcessor {
 		}
 		return false;
 	}
+	
+
+    @Override
+    public Set<String> getSupportedAnnotationTypes() {
+        Set<String> supportedAnnotationTypes = new HashSet<>();
+        supportedAnnotationTypes.add("*");
+        return supportedAnnotationTypes;
+    }
+
+	
+    @Override
+    public SourceVersion getSupportedSourceVersion() {
+        return SourceVersion.RELEASE_8;
+    }
 }

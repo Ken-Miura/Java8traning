e69cd4fb4e949c8bb8382ad30b5b9a26226d55f4
Ken-Miura <ken.miura1102@gmail.com>
2017-02-17 23:29:06 +0900

    コメント追記

--- src/ch07/ex03/ch07_ex03.js
+++ src/ch07/ex03/ch07_ex03.js
@@ -1,4 +1,5 @@
 // jjsに打ち込んだコマンドは以下の通り（jdk1.8.0_121に付属するjjsではテキストの問題のような奇妙な表示は起こらなかった。）
+// 特定のバージョン以下のjjsでは問題が発生するらしいが、jre1.8.0_102に含まれるjjsではすでにバグ修正済みらしく、問題は発生しない。
 var b = new java.math.BigInteger('1234567890987654321')
 print(b)
 print(b.mod(java.math.BigInteger.TEN))
--- src/ch07/ex04/ch07_ex04.js
+++ src/ch07/ex04/ch07_ex04.js
@@ -1,6 +1,4 @@
-#!/home/kmiura/java/jdk1.8.0_121/bin/jjs
-
 var StringClass = java.lang.String.class
 var s = 'Javascript'.substring(0, 4)
 print(s.getClass()) // 出力結果: class java.lang.String
-print(StringClass.cast(s.getClass()))　// 出力結果: java.lang.ClassCastException: Cannot cast java.lang.Class to java.lang.String /* Class型をString型にキャストしようとしているため、ClassCastExceptionがスローされる。*/
+print(StringClass.cast(s)) // 特定のバージョン以下のjjsでは問題が発生するらしいが、jre1.8.0_102に含まれるjjsではすでにバグ修正済みらしく、問題は発生しない。

a814c2851db2917d7b77d4731992ede64866d4a7
Ken-Miura <ken.miura1102@gmail.com>
2017-03-14 23:38:46 +0900

    例外処理

--- src/ch09/ex07/GetContentsToFile.java
+++ src/ch09/ex07/GetContentsToFile.java
@@ -5,16 +5,25 @@ package ch09.ex07;
 
 import java.io.IOException;
 import java.io.InputStream;
+import java.net.MalformedURLException;
 import java.net.URL;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 
 public final class GetContentsToFile {
 
-	public static void main(String[] args) throws IOException {
-		URL url = new URL("https://www.google.co.jp/");
+	public static void main(String[] args) {
+		URL url = null;
+		try {
+			url = new URL("https://www.google.co.jp/");
+		} catch (MalformedURLException e) {
+			e.printStackTrace();
+		}
+		
 		try (InputStream in = url.openStream();) {
 			Files.copy(in, Paths.get("output.html"));
+		} catch (IOException e) {
+			e.printStackTrace();
 		}
 	}
 
